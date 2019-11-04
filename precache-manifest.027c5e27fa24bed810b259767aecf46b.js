1892925e85fbe5ee5d5695d1a577e2dec0d4b5c0
mahdieh019 <mahsanasr2016@gmail.com>
2019-11-04 12:13:50 +0100

    Updates

---
+++ precache-manifest.027c5e27fa24bed810b259767aecf46b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e40a6133a8f19710f0cd9e0259fee74",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "1077c39d497b30cc6e08",
+    "url": "/deploy-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7cd7a9600825df5b2d1e",
+    "url": "/deploy-react/static/js/2.0015f55b.chunk.js"
+  },
+  {
+    "revision": "1077c39d497b30cc6e08",
+    "url": "/deploy-react/static/js/main.dd14b187.chunk.js"
+  },
+  {
+    "revision": "4e40f8f340481e686b55",
+    "url": "/deploy-react/static/js/runtime-main.e6db49c1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/deploy-react/static/media/logo.25bf045c.svg"
+  }
+]);
